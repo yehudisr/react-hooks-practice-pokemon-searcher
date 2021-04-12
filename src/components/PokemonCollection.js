ffbb7bb8d65c3be504f5a51556d8ce09d02d4da8
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-12 16:59:13 -0400

    all deliverables

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,14 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemonList}) {
+
+ const pokemonCollection = pokemonList.map((pokemon)=> <PokemonCard pokemon={pokemon}/>)
+ 
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {pokemonCollection}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
+const API = "http://localhost:3001/pokemon"
+
 function PokemonPage() {
+  const [pokemonList, setPokemonList] = useState([])
+  const [search, setSearch] = useState("")
+
+    useEffect(()=>{
+       fetch(API)
+       .then(resp => resp.json())
+       .then(data => setPokemonList(data))
+    },
+    [])
+
+    function handleSearch(event){
+      setSearch(event.target.value)
+    }
+    function handleAddPokemon(newpokemon){
+      setPokemonList([...pokemonList, newpokemon])
+    }
+
+    const filteredList = pokemonList
+    .filter((pokemon) => pokemon.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handleAddPokemon}/>
       <br />
-      <Search />
+      <Search value={search} onSearchChange={handleSearch} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemonList={filteredList} />
     </Container>
   );
 }
