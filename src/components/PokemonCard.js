46fbabe6c6dfe23ca62ac5f0d8a4f48b07fc3251
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-12 16:58:48 -0400

    commit tree fc975fbeb8ba79990f5af78c1848c29535575d1d

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({pokemon}) {
+
+  const [sprite, setSprite] = useState(false)
+  const { name, hp, sprites } = pokemon
+
+ function handleClick(){
+    setSprite((sprite) => !sprite)
+ }
+
   return (
     <Card>
       <div>
-        <div className="image">
-          <img alt="oh no!" />
+        <div className="image" onClick={handleClick}>
+          <img src={sprite ? sprites.front : sprites.back } alt={name} />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp} hp
           </span>
         </div>
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Search() {
+function Search({onSearchChange, search}) {
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input onChange={onSearchChange} value={search} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
